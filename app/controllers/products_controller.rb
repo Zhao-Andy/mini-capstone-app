b9fe32078b8dc243a2aad6808dda256d358487be
Andy Zhao <andyzhao.zhao@gmail.com>
2016-07-24 10:29:53 -0400

    Completed all CRUD methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,46 @@
 class ProductsController < ApplicationController
-  def products
+  def index
     @products = Product.all
+    render 'index.html.erb'
   end
 
-  def first_product
-    @first = Product.find_by(id: 1)
+  def create
+    @create = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image: params[:image],
+      desc: params[:desc]
+    )
+    @create.save
   end
 
-  def second_product
-    @second = Product.find_by(id: 2)
+  def new
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render 'show.html.erb'
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+    render 'edit.html.erb'
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(
+    name: params[:name],
+    price: params[:price],
+    image: params[:image],
+    desc: params[:desc]
+    )
+    render 'update.html.erb'
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render "destroy.html.erb"
   end
 end
