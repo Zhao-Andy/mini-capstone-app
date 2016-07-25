ff74654496d9a62c7c58dd1408c7004b350dbae7
Andy Zhao <andyzhao.zhao@gmail.com>
2016-07-25 19:24:07 -0400

    added tax and total methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ActiveRecord::Base
+  def sale_message
+    if price.to_i < 2
+      @message = "Discount Item!"
+    else
+      @message = "Not on Sale!"
+    end
+  end
+
+  def tax
+    (price.to_f * 0.09)
+  end
+
+  def total
+    (price.to_f + tax).round(2)
+  end
 end
